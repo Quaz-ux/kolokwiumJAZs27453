20746973e2c369b436874b908044c07a4d888222
Quaz-ux <szymon.anikiej@gmail.com>
2024-06-29 12:40:39 +0200

    pierwszy commit

---
+++ src/main/java/com/example/jazs27453nbp/JazS27453NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs27453nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS27453NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS27453NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jazs27453nbp/JazS27453NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs27453nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS27453NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
